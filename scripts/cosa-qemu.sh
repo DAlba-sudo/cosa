03b129bdc0f9d6c1a908c236394854962bd20c3e
DAlba-sudo <d.alba.work@gmail.com>
2023-09-14 11:25:10 -0400

    fixed gdb remote issue

---
+++ scripts/cosa-qemu.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+
+GDBFLAGS=""
+dummy=""
+
+while getopts "g" flag; do
+    case $flag in
+        g) # handle the gdb debugger activation command
+            GDBFLAGS="-S -s" 
+            echo "Starting $GDBFLAGS"
+            ;;
+    esac
+done
+
+qemu-system-i386 -nographic -drive file=./cosa.img,index=0,media=disk,format=raw -smp 1 -m 512 $gdbflags
